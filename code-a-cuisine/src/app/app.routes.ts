6fba4a6a48261a5c63a57a0708042099f68061b1
Max Lehner <dev@lm-lehner.at>
2026-09-13 10:11:01 +0200

    feat: clean up code formatting and improve SCSS structure for consistency

--- code-a-cuisine/src/app/app.routes.ts
+++ code-a-cuisine/src/app/app.routes.ts
@@ -1,4 +1,4 @@
 import { Routes } from '@angular/router';
-import {LandingPage} from './pages/home-page/landing-page';
+import { LandingPage } from './pages/home-page/landing-page';
 
 export const routes: Routes = [{ path: '', component: LandingPage }];
--- code-a-cuisine/src/app/pages/home-page/landing-page.ts
+++ code-a-cuisine/src/app/pages/home-page/landing-page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MainHeader } from '../../components/main-header/main-header';
-import {Button} from '../../components/button/button';
+import { Button } from '../../components/button/button';
 
 @Component({
   imports: [MainHeader, Button],
